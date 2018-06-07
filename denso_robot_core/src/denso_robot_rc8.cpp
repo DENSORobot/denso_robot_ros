d79d7d8b6c3babcdcb79e9223465259abd1de627
Toshitaka Suzuki <toshitaka.suzuki@denso-wave.co.jp>
2018-06-07 16:23:16 +0900

    Allow extended-joint motors to be controlled

--- denso_robot_core/src/denso_robot_rc8.cpp
+++ denso_robot_core/src/denso_robot_rc8.cpp
@@ -663,9 +663,9 @@ HRESULT DensoRobotRC8::CreateSendParameter(
       return E_FAIL;
   }
 
-  if(joints < pose.size()) {
-    return E_FAIL;
-  }
+  //if(joints < pose.size()) {
+  //  return E_FAIL;
+  //}
 
   // Check send format
   bool send_hio, send_mio, send_uio, recv_uio;
@@ -689,6 +689,8 @@ HRESULT DensoRobotRC8::CreateSendParameter(
   double *pdbl;
   uint8_t *pbool;
 
+  // Number of joints + option
+  joints += 1;
   if(num == 1) {
     // Pose only
     send->vt = (VT_ARRAY | VT_R8);
