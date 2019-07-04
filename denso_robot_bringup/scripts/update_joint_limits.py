1ca74760d534fbaa25c889b148e933635bf3173c
ToshitakaSuzuki <toshitaka.suzuki@denso-wave.co.jp>
2019-07-04 19:32:07 +0900

    Change to insert space after colon.

--- denso_robot_bringup/scripts/update_joint_limits.py
+++ denso_robot_bringup/scripts/update_joint_limits.py
@@ -75,13 +75,24 @@ if __name__ == "__main__":
     elem_joints = et_urdf.getroot().findall("joint")
 
     r_joint = re.compile("(joint_\d+):")
-    r_has_vel_limit = re.compile("has_velocity_limits:(\w+)")
-    r_max_vel = re.compile("max_velocity:([\d\.]+)")
+    r_has_vel_limit = re.compile("has_velocity_limits: (\w+)")
+    r_max_vel = re.compile("max_velocity: ([\d\.]+)")
     
     cur_elem = None
     new_joint = False
     
-    f_limits = open(path_limits, "r")
+    f_limits = open(path_limits, "r+")
+
+    # Insert space after colon.
+    data_lines = f_limits.read()
+    index = data_lines.find(": ")
+    if index == -1:
+        new_data = re.sub(r':([\w\.]+)', r': \1', data_lines)
+        f_limits.seek(0)
+        f_limits.write(new_data)
+        f_limits.truncate()
+
+    f_limits.seek(0)
     line = f_limits.readline()
     while line:
         m = r_joint.search(line)
